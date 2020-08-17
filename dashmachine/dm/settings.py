b67d70ad13ebe20b140ef68b2bef00c7e8d18888
ross <ross.mountjoy@gmail.com>
2020-08-17 09:42:22 -0400

    fixed issue with no settings crashing startup

--- dashmachine/dm/settings.py
+++ dashmachine/dm/settings.py
@@ -7,15 +7,16 @@ class Settings:
         self.error = None
         try:
             self.toml_dict = toml.load(settings_toml)
-            for k, v in self.toml_dict["Settings"].items():
-                setattr(self, k, v)
-
         except Exception as e:
             self.error = (
                 f"DashMachine was unable to read your settings.toml file. \n"
                 f"Here is error: {e}"
             )
             return
+
+        if self.toml_dict.get("Settings"):
+            for k, v in self.toml_dict["Settings"].items():
+                setattr(self, k, v)
 
         if not hasattr(self, "window"):
             self.window = {"title": "DashMachine"}
